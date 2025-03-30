bbb351eabb51bd0271eaa3bc6ee37da9c409ecb9
Marty <martina.creola2009@gmail.com>
2025-03-30 14:52:31 +0200

    Transizioni, animazioni e effetti

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Sito caricato correttamente!");
+
+document.addEventListener("DOMContentLoaded", function () {
+    const elements = document.querySelectorAll('.content, .contact');
+
+    function revealOnScroll() {
+        elements.forEach(element => {
+            const position = element.getBoundingClientRect().top;
+            const screenHeight = window.innerHeight;
+            
+            if (position < screenHeight - 50) {
+                element.classList.add('visible');
+            }
+        });
+    }
+
+    window.addEventListener('scroll', revealOnScroll);
+    revealOnScroll();
+});
